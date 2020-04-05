f37562a3ec5303431db0ca25d5d11751c0d2cfb0
Toan Nguyen <hgiasac@gmail.com>
2020-04-05 14:10:53 +0700

    update migration v2

--- services/events/src/shared/config.ts
+++ services/events/src/shared/config.ts
@@ -1,3 +1,5 @@
 export default {
-  debug: !!process.env.DEBUG || !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+  debug: !!process.env.DEBUG || !process.env.NODE_ENV || process.env.NODE_ENV === "development",
+  databaseURL: process.env.DATABASE_URL,
+  dataHost: process.env.DATA_HOST,
 };
